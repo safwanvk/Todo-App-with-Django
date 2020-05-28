d45221451d192b205f74e9c8f77bcbf010c46f15
safwan <safwanvalakindil@gmail.com>
2020-05-28 00:33:38 -0700

    Set static file

--- todo/settings.py
+++ todo/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'todo/static')
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
