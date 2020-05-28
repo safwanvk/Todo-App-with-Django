bf24d33433f58ede54dde5fe7cb5ca47d017f547
safwan <safwanvalakindil@gmail.com>
2020-05-27 23:00:12 -0700

    Create view

--- account/views.py
+++ account/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from account.forms import TodoForm
+from account.models import Todo
 
 
 def home(request):
     form = TodoForm()
+    todos = Todo.objects.all()
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
 
-    return render(request, 'home.html', {'form': form})
+    return render(request, 'home.html', {'form': form, 'todos': todos})
 
 
 def update(request):
